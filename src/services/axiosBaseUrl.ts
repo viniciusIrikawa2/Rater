ffad4f4958968d09bacfb1240b568f716d70f8ec
viniciusIrikawa2 <viniciusirikawa@gmail.com>
2024-08-08 19:20:41 -0300

    chore: axios interceptor

--- src/services/axiosBaseUrl.ts
+++ src/services/axiosBaseUrl.ts
@@ -3,3 +3,12 @@ import axios from "axios";
 export const BASEURL = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL
 })
+
+BASEURL.interceptors.request.use(config => {
+    if (import.meta.env.VITE_TOKEN) {
+        config.headers.Authorization = `Bearer ${import.meta.env.VITE_TOKEN}`;
+    }
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
