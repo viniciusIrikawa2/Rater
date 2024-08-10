e48c2d393b4cecd83c25ad5bdc2688106a7aaadf
viniciusIrikawa2 <viniciusirikawa@gmail.com>
2024-08-09 22:07:01 -0300

    feat: initializing the page ActorPage

---
+++ src/reading_pages/actor/ActorPage.tsx
@@ -0,0 +1,12 @@
+import { ActorWrapper, Container, MovieWrapper } from "./styles";
+
+const ActorPage = () => {
+  return (
+    <Container>
+        <MovieWrapper> movie wrapper </MovieWrapper>
+        <ActorWrapper> actor wrapper </ActorWrapper>
+    </Container>
+  )
+}
+
+export default ActorPage;
---
+++ src/reading_pages/actor/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    border: 2px solid red;
+`;
+
+export const MovieWrapper = styled.div`
+    border: 2px solid green;
+`;
+
+export const ActorWrapper = styled.div`
+    border: 2px solid purple;
+`;
