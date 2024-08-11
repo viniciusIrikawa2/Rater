1352fa23eb95a8e787e1fa130bf996c075a2b9fd
viniciusIrikawa2 <viniciusirikawa@gmail.com>
2024-08-11 15:11:22 -0300

    fix: removing console

--- src/functions/helpers.ts
+++ src/functions/helpers.ts
@@ -14,5 +14,5 @@ export const returnAge = (birthday: string) => {
 export const convertDurationToHours = (time: number): string => {
     const hours = Math.floor(time / 60);
     const minutes = time % 60;
-    return `${hours}h ${minutes}min`;
+    return `${hours}h ${minutes}m`;
 };
