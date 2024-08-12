cd84eeb92aa498ccd67d74466e3dec5985ae785b
viniciusIrikawa2 <viniciusirikawa@gmail.com>
2024-08-12 08:43:46 -0300

    feat: navigate to the actor's page

--- src/components/CelebritiesCard/CelebritiesCard.tsx
+++ src/components/CelebritiesCard/CelebritiesCard.tsx
@@ -7,19 +7,21 @@ import { actorImage } from "../../constants";
 import useFetchCelebrities from "../../hooks/useFetchCelebrities";
 import { Cast } from "../../@Types/credits";
 import { Actor } from "../../@Types/actors";
+import { useNavigate } from "react-router-dom";
 interface MovieCastProps {
   movieCast?: Cast[]
 }
 
 const CelebritiesCard = ({ movieCast }: MovieCastProps) => {
   const { celebrities, celebritiesBirthdays } = useFetchCelebrities(movieCast);
+  const navigate = useNavigate();
 
   return (
     <>
       <Swiper slidesPerView={5.5} spaceBetween={10}> 
         {celebrities.map((actor: Cast | Actor, index: number) => (
           <SwiperSlide key={actor.id}>
-            <Card imageUrl={`${actorImage}${actor.profile_path}`}>
+            <Card imageUrl={`${actorImage}${actor.profile_path}`} onClick={(e) => navigate(`/actor/${actor.id.toString()}`)}>
               <Wrapper>
                 <TitleWrapper>
                   <H3> {actor.name} </H3>
